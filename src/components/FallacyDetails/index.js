035a8449b9f71eff5d3509dbe2956703a8465ce6
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2023-12-29 07:19:56 +0330

    Extract FallacyDetails Component

---
+++ src/components/FallacyDetails/index.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { IoCloseOutline } from "react-icons/io5";
+import './FallacyDetails.css';
+
+const FallacyDetails = ({ selectedFallacy, setSelectedFallacy, showCopyModal }) => {
+    const fallacyDetailsWrapper = React.useRef(null);
+    const fallacyDetails = React.useRef(null);
+
+    React.useEffect(() => {
+        if (!selectedFallacy) return;
+        fallacyDetailsWrapper.current.style.visibility = 'visible';
+        fallacyDetailsWrapper.current.classList.remove('-hidden')
+      }, [selectedFallacy]);
+
+    React.useEffect(() => {
+        const handleFallacyDetailsTransitionEnd = () => {
+            const isClosed = fallacyDetailsWrapper.current.classList.contains('-hidden');
+            if (!isClosed) return;
+            fallacyDetailsWrapper.current.style.visibility = 'hidden';
+            setSelectedFallacy(null);
+          }
+        fallacyDetails.current.addEventListener('transitionend', handleFallacyDetailsTransitionEnd);
+        return () => {
+            fallacyDetails.current.removeEventListener('transitionend', handleFallacyDetailsTransitionEnd);
+        }
+    }, []);
+    
+      const hideFallacyDetails = () => {
+        fallacyDetailsWrapper.current.classList.add('-hidden');
+      }
+    return (
+        <div ref={fallacyDetailsWrapper} style={{ visibility: 'hidden' }} className="FallacyDetails__wrapper -hidden">
+            <div onClick={hideFallacyDetails} className="FallacyDetails__contentFader" />
+            <div ref={fallacyDetails} className="FallacyDetails">
+            <button onClick={hideFallacyDetails} className="FallacyDetails__buttonClose">
+                <IoCloseOutline size="3rem" />
+            </button>
+            <h2 className="FallacyDetails__title">{`مغالطه ${selectedFallacy?.title}`}</h2>
+            <p className="FallacyDetails__description"><span style={{ color: 'var(--color-primary)'}}>توضیح:‌ </span>{selectedFallacy?.description}</p>
+            {selectedFallacy?.examples?.map((example, index) => (
+                <div key={example.id} className="FallacyDetails__example">
+                <span>{`مثال ${index + 1}: `}<span className="FallacyDetails__exampleValue">{example.value}</span></span>
+                </div>
+            ))}
+            <button onClick={showCopyModal} className="FallacyDetails__buttonCopyMessage">کسی از این مغالطه استفاده می‌کند؟!</button>
+            </div>
+        </div>
+    );
+}
+
+export default FallacyDetails;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,15 @@
 import * as React from "react";
-import { IoCloseOutline } from "react-icons/io5";
 
 import './index.css';
 import Header from "../components/Header";
 import FallaciesList from "../components/FallaciesList";
+import FallacyDetails from "../components/FallacyDetails";
 
 const IndexPage = () => {
   const [searchValue, updateSearchValue] = React.useState('');
   const [selectedFallacy, setSelectedFallacy] = React.useState(null);
-  const fallacyDetailsWrapper = React.useRef(null);
-  const fallacyDetails = React.useRef(null);
   const copyModal = React.useRef(null);
   const copyModalText = React.useRef(null);
-  React.useEffect(() => {
-    if (!selectedFallacy) return;
-    fallacyDetailsWrapper.current.style.visibility = 'visible';
-    fallacyDetailsWrapper.current.classList.remove('-hidden')
-  }, [selectedFallacy]);
 
   const showCopyModal = () => {
     copyModal.current.style.visibility = 'visible';
@@ -31,16 +24,7 @@ const IndexPage = () => {
     }, 1000);
   };
 
-  const hideFallacyDetails = () => {
-    fallacyDetailsWrapper.current.classList.add('-hidden');
-  }
   React.useEffect(() => {
-    const handleFallacyDetailsTransitionEnd = () => {
-      const isClosed = fallacyDetailsWrapper.current.classList.contains('-hidden');
-      if (!isClosed) return;
-      fallacyDetailsWrapper.current.style.visibility = 'hidden';
-      setSelectedFallacy(null);
-    }
     const handleCopyModalTransitionEnd = () => {
       if (copyModal.current.classList.contains('-closed')) {
         copyModal.current.style.visibility = 'hidden';
@@ -50,33 +34,16 @@ const IndexPage = () => {
     }
 
     copyModal.current.addEventListener('transitionend', handleCopyModalTransitionEnd);
-    fallacyDetails.current.addEventListener('transitionend', handleFallacyDetailsTransitionEnd);
     return () => {
-      fallacyDetails.current.removeEventListener('transitionend', handleFallacyDetailsTransitionEnd);
       copyModal.current.removeEventListener('transitionend', handleCopyModalTransitionEnd);
     }
-  }, [])
+  }, []);
   return (
     <main>
       <Header searchValue={searchValue} updateSearchValue={updateSearchValue} />
       <FallaciesList searchValue={searchValue} setSelectedFallacy={setSelectedFallacy} />
       <div className="scrollFaderBottom"/>
-      <div ref={fallacyDetailsWrapper} style={{ visibility: 'hidden' }} className="fallacyDetailsWrapper -hidden">
-        <div onClick={hideFallacyDetails} className="contentFader" />
-        <div ref={fallacyDetails} className="fallacyDetails">
-          <button onClick={hideFallacyDetails} className="fallacyDetails__buttonClose">
-            <IoCloseOutline size="3rem" />
-          </button>
-          <h2 className="fallacyDetails__title">{`مغالطه ${selectedFallacy?.title}`}</h2>
-          <p className="fallacyDetails__description"><span style={{ color: 'var(--color-primary)'}}>توضیح:‌ </span>{selectedFallacy?.description}</p>
-          {selectedFallacy?.examples?.map((example, index) => (
-            <div key={example.id} className="fallacyDetails__example">
-              <span>{`مثال ${index + 1}: `}<span className="fallacyDetails__exampleValue">{example.value}</span></span>
-            </div>
-          ))}
-          <button onClick={showCopyModal} className="fallacyDetails__buttonCopyMessage">کسی از این مغالطه استفاده می‌کند؟!</button>
-        </div>
-      </div>
+      <FallacyDetails selectedFallacy={selectedFallacy} setSelectedFallacy={setSelectedFallacy} showCopyModal={showCopyModal} />
       <div style={{ visibility: 'hidden'}} ref={copyModal} className="copyModal -closed">
         <div ref={copyModalText} className="copyModal__textContainer">
           <p>متنی کپی شد !</p>
