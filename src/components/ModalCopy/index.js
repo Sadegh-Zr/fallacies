8363b28198fcd557d94849c24977927ccf9e5b01
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2023-12-29 07:32:27 +0330

    Extract ModalCopy Component

---
+++ src/components/ModalCopy/index.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import './ModalCopy.css';
+
+const ModalCopy = ({ isOpen, selectedFallacy, hideModal }) => {
+    const container = React.useRef(null);
+    const textContainer = React.useRef(null);
+    React.useEffect(() => {
+    if(isOpen) {
+        container.current.style.visibility = 'visible';
+        container.current.classList.remove('-closed');
+        textContainer.current.classList.add('-scaleIn');
+        navigator.clipboard.writeText(`با احترام، به نظر می‌رسد که کلام شما دچار مغالطه ${selectedFallacy.title} شده است.`)
+        setTimeout(() => {
+            textContainer.current.classList.add('-scaleOut');
+            setTimeout(() => {
+            container.current.classList.add('-closed');
+            }, 150);
+        }, 1000);
+    };
+    }, [isOpen]);
+
+    React.useEffect(() => {
+        const handleTransitionEnd = () => {
+          if (container.current.classList.contains('-closed')) {
+            container.current.style.visibility = 'hidden';
+            textContainer.current.classList.remove('-scaleOut');
+            textContainer.current.classList.remove('-scaleIn');
+            hideModal();
+          }
+        }
+    
+        container.current.addEventListener('transitionend', handleTransitionEnd);
+        return () => {
+          container.current.removeEventListener('transitionend', handleTransitionEnd);
+        }
+      }, []);
+    return (
+        <div style={{ visibility: 'hidden'}} ref={container} className="ModalCopy -closed">
+            <div ref={textContainer} className="ModalCopy__textContainer">
+            <p>متنی کپی شد !</p>
+            <p>آن را برای شخص مغالط ارسال کنید :)</p>
+            </div>
+        </div>
+    )
+};
+
+export default ModalCopy;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,56 +4,24 @@ import './index.css';
 import Header from "../components/Header";
 import FallaciesList from "../components/FallaciesList";
 import FallacyDetails from "../components/FallacyDetails";
+import ModalCopy from "../components/ModalCopy";
 
 const IndexPage = () => {
   const [searchValue, updateSearchValue] = React.useState('');
   const [selectedFallacy, setSelectedFallacy] = React.useState(null);
-  const copyModal = React.useRef(null);
-  const copyModalText = React.useRef(null);
+  const [isModalCopyVisible, setModalCopyVisibility] = React.useState(false);
 
-  const showCopyModal = () => {
-    copyModal.current.style.visibility = 'visible';
-    copyModal.current.classList.remove('-closed');
-    copyModalText.current.classList.add('-scaleIn');
-    navigator.clipboard.writeText(`با احترام، به نظر می‌رسد که کلام شما دچار مغالطه ${selectedFallacy.title} شده است.`)
-    setTimeout(() => {
-      copyModalText.current.classList.add('-scaleOut');
-      setTimeout(() => {
-        copyModal.current.classList.add('-closed');
-      }, 150);
-    }, 1000);
-  };
-
-  React.useEffect(() => {
-    const handleCopyModalTransitionEnd = () => {
-      if (copyModal.current.classList.contains('-closed')) {
-        copyModal.current.style.visibility = 'hidden';
-        copyModalText.current.classList.remove('-scaleOut');
-        copyModalText.current.classList.remove('-scaleIn');
-      }
-    }
-
-    copyModal.current.addEventListener('transitionend', handleCopyModalTransitionEnd);
-    return () => {
-      copyModal.current.removeEventListener('transitionend', handleCopyModalTransitionEnd);
-    }
-  }, []);
   return (
     <main>
       <Header searchValue={searchValue} updateSearchValue={updateSearchValue} />
       <FallaciesList searchValue={searchValue} setSelectedFallacy={setSelectedFallacy} />
       <div className="scrollFaderBottom"/>
-      <FallacyDetails selectedFallacy={selectedFallacy} setSelectedFallacy={setSelectedFallacy} showCopyModal={showCopyModal} />
-      <div style={{ visibility: 'hidden'}} ref={copyModal} className="copyModal -closed">
-        <div ref={copyModalText} className="copyModal__textContainer">
-          <p>متنی کپی شد !</p>
-          <p>آن را برای شخص مغالط ارسال کنید :)</p>
-        </div>
-      </div>
+      <FallacyDetails selectedFallacy={selectedFallacy} setSelectedFallacy={setSelectedFallacy} showCopyModal={() => { setModalCopyVisibility(true); }} />
+      <ModalCopy isOpen={isModalCopyVisible} selectedFallacy={selectedFallacy} hideModal={() => { setModalCopyVisibility(false); }} />
     </main>
   )
 }
 
-export default IndexPage
+export default IndexPage;
 
 export const Head = () => <title>مغالطات</title>
