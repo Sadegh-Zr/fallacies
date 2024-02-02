91250442c6d75c0fdc098878f8bf2d0ed51082e8
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-02 17:08:26 +0330

    Implement Opening Fallacy From Link

--- src/components/FallaciesList/index.js
+++ src/components/FallaciesList/index.js
@@ -6,27 +6,37 @@ import { isBrowser } from '../../utils';
 
 const FallaciesList = ({ setSelectedFallacy, searchValue }) => {
 
+  const searchParams = new URLSearchParams(isBrowser ? window.location.search : '');
   const remindingFallaciesString = isBrowser ? localStorage.getItem('remindingFallacies') : '';
-    const remindingFallacies = remindingFallaciesString ? JSON.parse(remindingFallaciesString) : [];
-    const renderFallacies = () => {
-        const data = fallaciesJSON.data.filter(({ title }) => title.includes(searchValue));
-        if (!data.length) {
-          return (
-            <div className="FallaciesList__notFound">
-              <RiMenuSearchLine size="5rem" />
-              <p>مغالطه‌ای با این نام یافت نشد...</p>
-            </div>
-          )
-        };
-        const handleClick = fallacy => {
-          const newList = remindingFallacies.filter(fallacyId => fallacyId !== fallacy.id)
-          localStorage.setItem('remindingFallacies', JSON.stringify(newList));
-          setSelectedFallacy(fallacy);
-        }
-        return data.map(fallacy => (
-          <button onClick={() => { handleClick(fallacy) }} key={fallacy.id}>{remindingFallacies.includes(fallacy.id) && <span />} {`مغالطه ${fallacy.title}`}</button>
-        ))
+  const remindingFallacies = remindingFallaciesString ? JSON.parse(remindingFallaciesString) : [];
+
+  React.useEffect(() => {
+    const fallacyId = searchParams.get('fid');
+    if (fallacyId) {
+      setSelectedFallacy(fallaciesJSON.data.find(fallacy => fallacy.id === Number(fallacyId)));
+    }
+  }, []);
+
+  const handleClick = fallacy => {
+    const newList = remindingFallacies.filter(fallacyId => fallacyId !== fallacy.id)
+    localStorage.setItem('remindingFallacies', JSON.stringify(newList));
+    setSelectedFallacy(fallacy);
+  }
+
+  const renderFallacies = () => {
+      const data = fallaciesJSON.data.filter(({ title }) => title.includes(searchValue));
+      if (!data.length) {
+        return (
+          <div className="FallaciesList__notFound">
+            <RiMenuSearchLine size="5rem" />
+            <p>مغالطه‌ای با این نام یافت نشد...</p>
+          </div>
+        )
       };
+      return data.map(fallacy => (
+        <button onClick={() => { handleClick(fallacy) }} key={fallacy.id}>{remindingFallacies.includes(fallacy.id) && <span />} {`مغالطه ${fallacy.title}`}</button>
+      ))
+    };
     return (
         <div className="FallaciesList">
             {renderFallacies()}
