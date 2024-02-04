cad5c9e3e7152ce36035795bbdabfdba97bbdc94
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-04 16:12:32 +0330

    Add Screenshots

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,19 +21,49 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-      siteUrl: `https://sadegh-zr.github.io/fallacies`,
-      name: `مغالطات`,
-      description: "مرجعی برای تسلط به مغالطات به همراه مثال‌های کاربردی و آزمون‌گیری",
-      short_name: `مغالطات`,
-      start_url: `/`,
-      background_color: `#fbc531`,
-      theme_color: `#fbc531`,
-      display: `standalone`,
-      theme_color_in_head: false,
-      icon: "./src/images/icon.svg",
-      icon_options: {
-          purpose: `any maskable`,
-        },
+        siteUrl: `https://sadegh-zr.github.io/fallacies`,
+        name: `مغالطات`,
+        description: "مرجعی برای تسلط به مغالطات به همراه مثال‌های کاربردی و آزمون‌گیری",
+        short_name: `مغالطات`,
+        start_url: `/`,
+        background_color: `#fbc531`,
+        theme_color: `#fbc531`,
+        display: `standalone`,
+        theme_color_in_head: false,
+        screenshots: [
+          {
+            src: "./src/images/screenshot1.jpg",
+            sizes: "591x1280",
+            type: "image/jpeg",
+            label: "صفحه اصلی با مشخص بودن مغالطاتی که نیاز به مرور دارند"
+          },
+          {
+            src: "./src/images/screenshot2.jpg",
+            sizes: "591x1280",
+            type: "image/jpeg",
+            label: "توضیحات و مثال‌های هر مغالطه",
+          },
+          {
+            src: "./src/images/screenshot3.jpg",
+            sizes: "591x1280",
+            type: "image/jpeg",
+            label: "کسی دچار مغالطه خاصی شده است؟ متنی که اپلیکیشن برای شما تولید می‌کند برای او بفرستید!"
+          },
+          {
+            src: "./src/images/screenshot4.jpg",
+            sizes: "591x1280",
+            type: "image/jpeg",
+            label: "بخش آزمون با قابلیت نمایش خطاها و مرور آن‌ها"
+          },
+          {
+            src: "./src/images/screenshot5.jpg",
+            sizes: "591x1280",
+            type: "image/jpeg",
+            label: "صفحه نتایج آزمون"
+          },
+        ],
+        icon: "./src/images/icon.svg",
+
       },
     },
     'gatsby-plugin-offline'
