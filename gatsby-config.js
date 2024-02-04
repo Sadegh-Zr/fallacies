a29a7c50f266cc9d5e184fb3995e79a8ab3fcfa4
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-04 15:16:18 +0330

    Fix Description Meta Bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,14 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const siteUrl = 'https://sadegh-zr.github.io/fallacies';
+
 module.exports = {
   pathPrefix: "/fallacies",
   siteMetadata: {
-    siteUrl: `https://sadegh-zr.github.io/fallacies`,
     title: "مغالطات",
-    description: "مرجعی برای تسلط به مغالطات به همراه مثال‌های کاربردی و آزمون‌گیری",
+    siteUrl
   },
   plugins: [
     {
@@ -19,15 +21,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `مغالطات`,
-        short_name: `مغالطات`,
-        start_url: `/`,
-        background_color: `#fbc531`,
-        theme_color: `#fbc531`,
-        display: `standalone`,
-        theme_color_in_head: false,
-        icon: "./src/images/icon.svg",
-        icon_options: {
+      siteUrl: `https://sadegh-zr.github.io/fallacies`,
+      name: `مغالطات`,
+      description: "مرجعی برای تسلط به مغالطات به همراه مثال‌های کاربردی و آزمون‌گیری",
+      short_name: `مغالطات`,
+      start_url: `/`,
+      background_color: `#fbc531`,
+      theme_color: `#fbc531`,
+      display: `standalone`,
+      theme_color_in_head: false,
+      icon: "./src/images/icon.svg",
+      icon_options: {
           purpose: `any maskable`,
         },
       },
