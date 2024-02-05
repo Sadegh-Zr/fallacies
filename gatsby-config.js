ea46995ca4cd95bf805ea20aee0ced610d3b220e
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-05 12:26:54 +0330

    Configure Hybrid Icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,9 +102,30 @@ module.exports = {
           },
         ],
         icon: "./src/images/icon.svg",
-        icon_options: {
-          purpose: `any maskable`,
-        },
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+            purpose: 'maskable',
+          },
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+            purpose: 'maskable',
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ]
 
       },
     },
