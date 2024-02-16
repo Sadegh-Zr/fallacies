f30874f178b292bb42e43d7a560b842a9cdbfa99
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-16 15:48:01 +0330

    Improve ModalInfo UX

--- src/components/ModalInfo/index.js
+++ src/components/ModalInfo/index.js
@@ -8,13 +8,18 @@ const ModalInfo = ({ isVisible, toggle }) => {
     return (
         <Rodal visible={isVisible} leaveAnimation='door' onClose={toggle} showCloseButton={false}>
         <div className='ModalInfo'>
-            <button aria-label='بستن' className="ModalInfo__closeIconButton" onClick={toggle}>
-                <IoCloseOutline size="3rem" />
-            </button>
-            <h1>درباره اپلیکیشن</h1>
-            <p>با سلام! این برنامه را با توجه به کتاب «مغالطات» دکتر علی اصغر خندان طراحی کردم. علاوه بر استفاده از مثال‌ها و سؤالات خود کتاب که با توجه به آنچه صلاح به نظر می‌رسید، تغییراتی در برخی از آن‌ها دادم، بعضی مثال‌ها و سؤالات را نیز به محتوا افزودم. امیدوارم که این مجموعه مورد رضایت امام زمان -عجل الله تعالی فرجه الشریف- قرار بگیرد.</p>
-            <p>این برنامه به صورت PWA <a rel="noreferrer" href='https://github.com/Sadegh-Zr/fallacies' target='_blank'>توسعه داده شده است</a>؛ بنابراین می‌توانید به راحتی آن را نصب کرده و به شکل آفلاین از آن استفاده کنید.</p>
-            <p>برای نظرات، پیشنهادات و انتقادات، می‌توانید از طریق <a rel="noreferrer" href='mailto:sadegh.zarinmehr@gmail.com' target='_blank'>ایمیل</a> و <a rel="noreferrer" href='https://eitaa.com/sadegh_zr' target='_blank'>پیامبرسان ایتا</a> با من در ارتباط باشید.</p>
+            <div className='ModalInfo__titleContainer'>
+                <div>
+                    <button aria-label='بستن' className="ModalInfo__closeIconButton" onClick={toggle}>
+                        <IoCloseOutline size="3rem" />
+                    </button>
+                </div>
+                <h1>درباره اپلیکیشن</h1>
+                <div />
+            </div>
+            <p>با سلام! این برنامه را با توجه به توضیحات، مثال‌ها و سؤالات کتاب پرمغز «مغالطات» دکتر علی اصغر خندان طراحی کردم. علاوه بر استفاده از مطالب خود کتاب و تغییر در برخی از آن‌ها بحسب صلاحدید، بعضی مثال‌ها و سؤالات را نیز به محتوا افزودم. امیدوارم که این مجموعه مورد رضایت امام زمان -عجل الله تعالی فرجه الشریف- قرار بگیرد.</p>
+            <p>این برنامه به صورت PWA <a rel="noreferrer" href='https://github.com/Sadegh-Zr/fallacies' target='_blank'>به شکل اوپن سورس توسعه داده شده است</a>؛ بنابراین می‌توانید به راحتی آن را نصب کرده و به شکل آفلاین از آن استفاده کنید.</p>
+            <p>برای نظرات، پیشنهادات و انتقادات، می‌توانید از طریق <a rel="noreferrer" href='mailto:sadegh.zarinmehr@gmail.com' target='_blank'>ایمیل</a> يا <a rel="noreferrer" href='https://eitaa.com/sadegh_zr' target='_blank'>پیامبرسان ایتا</a> با من در ارتباط باشید.</p>
             <button aria-label='بسیار خب!' className='ModalInfo__close' onClick={toggle}>بسیار خب!</button>
         </div>
     </Rodal>
