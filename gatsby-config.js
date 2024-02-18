a0a4c5abf5ea22ad5e85208e24e257c3846f505a
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-18 07:55:38 +0330

    Update Accent Color In Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        color: `#3498db`,
+        color: `#0C356A`,
         showSpinner: false,
       },
     },
