b058fe74a782d03670dc55047c8737aa47d58ea9
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-25 21:50:19 +0330

    Update og:title Meta Tag

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -8,7 +8,7 @@ const Seo = ({ title, description }) => {
             <meta name="google" content="notranslate" />
             <meta name="theme-color" content="#fbc531" />
             <meta name="description" content={description}/>
-            <meta property="og:locale" content={language === 'en' ? 'en_US' : 'fa_IR'} />
+            <meta property="og:locale" content="fa_IR" />
             <meta property="og:title" content={title} />
             <meta name="og:description" content={description} />
             <meta property="og:image"  content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb" />
