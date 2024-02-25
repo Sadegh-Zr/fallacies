98f24f425f3853e2e0d14385560eaad80de3b1ea
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-25 22:11:08 +0330

    Remove Corner Radius From Meta Images

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -11,8 +11,8 @@ const Seo = ({ title, description }) => {
             <meta property="og:locale" content="fa_IR" />
             <meta property="og:title" content={title} />
             <meta name="og:description" content={description} />
-            <meta property="og:image"  content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb" />
-            <meta name="twitter:image" content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb" />
+            <meta property="og:image"  content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/2b67a344-f446-4e0e-890a-1110ae817e0d" />
+            <meta name="twitter:image" content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/2b67a344-f446-4e0e-890a-1110ae817e0d" />
         </>
     )
 };
