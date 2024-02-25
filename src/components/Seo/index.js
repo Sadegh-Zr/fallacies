5af39383a60afae1492dff91ec1c39ddf544d4c9
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-02-25 21:49:04 +0330

    Update Meta Tags

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -8,8 +8,11 @@ const Seo = ({ title, description }) => {
             <meta name="google" content="notranslate" />
             <meta name="theme-color" content="#fbc531" />
             <meta name="description" content={description}/>
-            <meta property="og:image"  content="https://github-production-user-asset-6210df.s3.amazonaws.com/93543701/305609046-bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240225T175336Z&X-Amz-Expires=300&X-Amz-Signature=faf06326197f071b04af66df5700d87fc469c83b9e67da1f52985dc71dac0647&X-Amz-SignedHeaders=host&actor_id=93543701&key_id=0&repo_id=736850156" />
-            <meta name="twitter:image" content="https://github-production-user-asset-6210df.s3.amazonaws.com/93543701/305609046-bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240225T175336Z&X-Amz-Expires=300&X-Amz-Signature=faf06326197f071b04af66df5700d87fc469c83b9e67da1f52985dc71dac0647&X-Amz-SignedHeaders=host&actor_id=93543701&key_id=0&repo_id=736850156" />
+            <meta property="og:locale" content={language === 'en' ? 'en_US' : 'fa_IR'} />
+            <meta property="og:title" content={title} />
+            <meta name="og:description" content={description} />
+            <meta property="og:image"  content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb" />
+            <meta name="twitter:image" content="https://github.com/Sadegh-Zr/fallacies/assets/93543701/bc82423e-66e9-4f4c-bcbe-24cfbf3a9ecb" />
         </>
     )
 };
